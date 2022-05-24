0d84463383aec9aa119a59276a97fade86126087
Mariusz Błaszczak <mariusz.blaszczak@akra.net>
2022-05-24 12:33:16 +0200

    Require auth on all page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV.fetch("USER"), password: ENV.fetch("PASSWORD"), except: :index
+  http_basic_authenticate_with name: ENV.fetch("USER"), password: ENV.fetch("PASSWORD")
 end
