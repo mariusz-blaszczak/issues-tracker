e737be9ee3921bcea484faafe4103339cf24b1ab
Mariusz Błaszczak <mariusz.blaszczak@akra.net>
2022-06-22 13:53:20 +0200

    colorize answered

--- app/helpers/today_entries_helper.rb
+++ app/helpers/today_entries_helper.rb
@@ -1,2 +1,14 @@
 module TodayEntriesHelper
+
+  def colorize_answered(issue, answer)
+    if today_entry_already_answered?(issue, answer)
+      "background: green"
+    end
+  end
+
+  private
+
+  def today_entry_already_answered?(issue, answer)
+    Entry.where(issue: issue, answer: answer).last&.observed_at&.today?
+  end
 end
